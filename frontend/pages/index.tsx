ea10ed5ae2234407c90e25a11f811c931f4b720f
Koori SW <koorimizuw@yahoo.co.jp>
2023-01-20 18:55:58 +0900

    feat: welcome page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,63 @@
+import styled from "@emotion/styled";
+import { Box, Button, Typography } from "@mui/material";
 import type { NextPage } from "next";
 
+const MyBox = styled(Box)`
+  margin: 60px 0;
+  a {
+    justify-content: center;
+  }
+`;
+
+const MyButton = styled(Button)`
+  margin-top: 10px;
+  width: 100%;
+  border-radius: 10px;
+`;
+
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <MyBox>
+        <Typography
+          variant="h2"
+          noWrap
+          component="a"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Welcome to
+        </Typography>
+        <Typography
+          variant="h2"
+          noWrap
+          component="a"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Attractive Life
+        </Typography>
+      </MyBox>
+      <MyButton href="/dashboard" variant="outlined" size="large">
+        Dashboard
+      </MyButton>
+      <MyButton href="/link" variant="outlined" size="large">
+        Link
+      </MyButton>
+    </>
+  );
 };
 
 export default Home;
