2db0e3177c7fa80f8f07191190d6c516a9b00ac6
Koori SW <koorimizuw@yahoo.co.jp>
2023-01-20 21:03:37 +0900

    feat: add title

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head'
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <title>Attractive Life</title>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 }
 
